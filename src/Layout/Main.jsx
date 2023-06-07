cf7b18699cc3dbb73e7bf93bae390c24ebca0d84
arafataft <arafatcd@gmail.com>
2023-06-08 02:11:09 +0600

    add navBar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import CssBaseline from '@mui/material/CssBaseline';
 import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/NavBar/NavBar";
 
 const Main = () => {
     return (
         <>
             <CssBaseline/>
+            <NavBar/>
             <Outlet></Outlet>
             <Footer/>
         </>
