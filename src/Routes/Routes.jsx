8249d0cad4d9076a7e435647c83f467253f5bda0
arafataft <arafatcd@gmail.com>
2023-06-09 15:56:59 +0600

    dashboard

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,6 +7,8 @@ import Home from "../Pages/Home/Home/Home";
 import Instructors from "../Pages/Instructors/Instructors";
 import Classes from "../Pages/Classes/Classes";
 import Register from "../Pages/Register/Register";
+import Dashboard from "../Pages/Dashboard/Dashboard/Dashboard";
+import AdminHome from "../Pages/Dashboard/AdminDashboard/AdminHome/AdminHome";
 
 export const router = createBrowserRouter([
     {
@@ -32,6 +34,16 @@ export const router = createBrowserRouter([
         {
           path:'signup',
           element:<Register/>
+        },
+        {
+          path:'dashboard',
+          element:<Dashboard/>,
+          children:[
+            {
+              path:'',
+              element:<AdminHome/>
+            }
+          ]
         }
       ]
     },
