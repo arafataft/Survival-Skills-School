716a3090c59232e0296b1f5fff841b8cca7ca003
arafataft <arafatcd@gmail.com>
2023-06-14 02:40:41 +0600

    make extra section

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,135 @@
+import { Grid, Container, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+const GallerySection = styled('div')(({ theme }) => ({
+//   backgroundColor: theme.palette.background.paper,
+  padding: theme.spacing(4, 0),
+}));
+
+const SectionTitle = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(4),
+  textAlign: 'center',
+}));
+
+const GalleryImageWrapper = styled('div')({
+  position: 'relative',
+  '&:hover $galleryImageOverlay': {
+    opacity: 1,
+  },
+});
+
+const GalleryImage = styled('img')({
+  width: '100%',
+  height: 'auto',
+  borderRadius: '4px',
+});
+
+const GalleryImageOverlay = styled('div')(() => ({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  opacity: 0,
+  transition: 'opacity 0.3s ease',
+}));
+
+const GalleryImageCaption = styled(Typography)(() => ({
+//   color: theme.palette.common.white,
+  fontWeight: 'bold',
+}));
+
+const Gallery = () => {
+  const images = [
+    {
+        id: 1,
+        src: 'https://source.unsplash.com/random/800x600/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 2,
+        src: 'https://source.unsplash.com/random/800x601/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 3,
+        src: 'https://source.unsplash.com/random/800x602/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 4,
+        src: 'https://source.unsplash.com/random/800x603/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 5,
+        src: 'https://source.unsplash.com/random/800x604/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 6,
+        src: 'https://source.unsplash.com/random/800x605/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 7,
+        src: 'https://source.unsplash.com/random/800x606/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 8,
+        src: 'https://source.unsplash.com/random/800x607/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 9,
+        src: 'https://source.unsplash.com/random/800x608/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 10,
+        src: 'https://source.unsplash.com/random/800x610/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 11,
+        src: 'https://source.unsplash.com/random/800x611/?survive-camp',
+        alt: 'Lego Toys',
+      },
+      {
+        id: 12,
+        src: 'https://source.unsplash.com/random/800x612/?survive-camp',
+        alt: 'Lego Toys',
+      },
+  ];
+
+  return (
+    <GallerySection>
+      <Container>
+        <SectionTitle variant="h4" component="h2">
+          Our Gallery
+        </SectionTitle>
+        <Grid container spacing={2}>
+          {images.map((image) => (
+            <Grid item xs={6} sm={3} key={image.id}>
+              <GalleryImageWrapper>
+                <GalleryImage src={image.src} alt={image.alt} />
+                <GalleryImageOverlay>
+                  <GalleryImageCaption variant="subtitle1">
+                    {image.alt}
+                  </GalleryImageCaption>
+                </GalleryImageOverlay>
+              </GalleryImageWrapper>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </GallerySection>
+  );
+};
+
+export default Gallery;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { Banner } from "../Banner/Banner";
+import Gallery from "../Gallery/Gallery";
 import PopularClasses from "../PopularClasses/PopularClasses";
 import PopularInstructors from "../PopularInstructors/PopularInstrutors";
 
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner/>
             <PopularClasses/>
             <PopularInstructors/>
+            <Gallery/>
         </div>
     );
 };
