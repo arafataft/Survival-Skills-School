9a5dd57266687c8a805d6056b62e9eb78696ed7c
arafataft <arafatcd@gmail.com>
2023-06-14 10:08:58 +0600

    display popular class

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -6,64 +6,58 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { Container } from '@mui/material';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+// import useAxiosSecure from 'path/to/useAxiosSecure';
 
 export default function PopularClasses() {
-    const [classData, setClassData] = useState(null);
+  const [classData, setClassData] = useState(null);
+  const { axiosSecure } = useAxiosSecure();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('classes.json'); 
-                if (!response.ok) {
-                    throw new Error('Failed to fetch class data');
-                }
-                const data = await response.json();
-                setClassData(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosSecure.get('classes');
+        setClassData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, [axiosSecure]);
 
-    // Filter and sort the class data based on available seats
-    const sortedClassData = classData?.filter((classItem) => classItem.availableSeats > 0).sort((a, b) => a.availableSeats - b.availableSeats);
+  const sortedClassData = classData?.filter((classItem) => classItem.availableSeats > 0).sort((b, a) => a.enroll - b.enroll);
 
-    // Display only the first 6 cards with the lowest available seats
-    const displayedClassData = sortedClassData?.slice(0, 6);
+  const displayedClassData = sortedClassData?.slice(0, 6);
 
-    return (
+  return (
+    <Box my={7}>
         <Container>
-            <Box display="flex" flexDirection="column" alignItems="center">
-                <Typography variant="h4" gutterBottom>
-                    Popular Classes
-                </Typography>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography variant="h4" gutterBottom>
+          Popular Classes
+        </Typography>
 
-                <Grid container spacing={2}>
-                    {displayedClassData &&
-                        displayedClassData.map((classItem) => (
-                            <Grid item xs={12} sm={6} md={4} key={classItem._id}>
-                                <Card sx={{ maxWidth: 345 }}>
-                                    <CardMedia
-                                        component="img"
-                                        height="140"
-                                        image={classItem.image}
-                                        alt={classItem.name}
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="body" component="div">
-                                            {classItem.name}
-                                        </Typography>
-                                        <Typography variant="body2" color="text.secondary">
-                                            Available Seats: {classItem.availableSeats}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        ))}
-                </Grid>
-            </Box>
-        </Container>
-    );
+        <Grid container spacing={2}>
+          {displayedClassData &&
+            displayedClassData.map((classItem) => (
+              <Grid item xs={12} sm={6} md={4} key={classItem._id}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia component="img" height="140" image={classItem.classImage} alt={classItem.className} />
+                  <CardContent>
+                    <Typography gutterBottom variant="body" component="div">
+                      {classItem.className}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Available Seats: {classItem.availableSeats}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+      </Box>
+    </Container>
+    </Box>
+  );
 }
